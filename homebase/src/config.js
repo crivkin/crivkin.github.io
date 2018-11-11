25e0f96e56995e752d9b43791f356282d9335645
Jack Steinberg <jacksnyny@gmail.com>
2018-11-11 15:54:34 -0500

    Added more comprehensive language support

--- homebase/src/config.js
+++ homebase/src/config.js
@@ -13,6 +13,30 @@ export const LANGUAGES = {
 				"en": "Transfer",
 				"es": "Enviar Dinero"
 			}
+		},
+		"card_descriptions": {
+			"resources": {
+				"en": `The Resources page hosts helpful links and documents for advancing understanding 
+                        of the immigration process, understanding of legal rights as an immigrant, and 
+                        understanding of availability of help.`,
+				"es": `La página Recursos organiza enlaces y documentos para avanzar la comprensión del 
+						proceso de inmigración, la comprensión de los derechos que tienen los inmigrantes,
+						 y la comprensión del disponibilidad de ayuda.`
+			},
+			"checklist": {
+				"en": `The Checklist page hosts a list of steps you must complete and links to documents 
+                        you must complete in order to navigate the immigration process, in the order you 
+                        must complete them.`,
+				"es": `La página de Cosas Que Hacer organiza una lista de paso que tiene que hacer y enlaces 
+						que tiene que completar para navegar el proceso de inmigración, en el orden que tiene 
+						que completarlos.`
+			},
+			"transfer": {
+				"en": `The Transfer page, using the TransferWise API, allows you to financially support 
+                        your loved ones back home by sending money back home cheaply and quickly.`,
+				"es": `La página de Enviar Dinero, que usa el API TransferWise, permite apoyar su familia 
+						en su país de origen por enviar dinero a ellos por un proceso barato y rápido.`
+			}
 		}
 	},
 	"checklist_page": {
@@ -20,5 +44,21 @@ export const LANGUAGES = {
 	},
 	"resources_page": {
 
+	},
+	"topBar": {
+		"dropdown_title": {
+			"en": "Language",
+			"es": "Idioma"
+		},
+		"languages": {
+			"en": {
+				"en": "English",
+				"es": "Inglés"
+			},
+			"es": {
+				"en": "Spanish",
+				"es": "Español"
+			}
+		}
 	}
 };
