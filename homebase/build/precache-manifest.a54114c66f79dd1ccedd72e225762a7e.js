919f2a1d0296b22dccd3ba9a95d69b18436d41fe
Charles Rivkin <crivkin@umich.edu>
2018-11-11 16:00:16 -0500

    build ish going up

---
+++ homebase/build/precache-manifest.a54114c66f79dd1ccedd72e225762a7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a83a4cc7d453048342ec",
+    "url": "/static/js/main.a83a4cc7.chunk.js"
+  },
+  {
+    "revision": "b1aaab8706e7d65d94ca",
+    "url": "/static/js/1.b1aaab87.chunk.js"
+  },
+  {
+    "revision": "a83a4cc7d453048342ec",
+    "url": "/static/css/main.edd0d7fa.chunk.css"
+  },
+  {
+    "revision": "f6ab7b089e04ab52cf13dacae93a5bd8",
+    "url": "/index.html"
+  }
+];
